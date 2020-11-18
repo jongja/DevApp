8f5c60b6b9dfcbd62cc5be2bccf0a9c5da2a7546
jongja <whwhdgus94@naver.com>
2020-11-18 22:50:07 +0900

    Update click app

--- Practice/app/src/main/java/com/example/practiceapp/MainActivity.kt
+++ Practice/app/src/main/java/com/example/practiceapp/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.practiceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.clickButton).setOnClickListener {
+            var tv = findViewById(R.id.textView4) as TextView
+            tv.setText("wow!")
+            tv.set
+        }
     }
-
-
 }
