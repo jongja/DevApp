95300129bd817d33467b445583bfd818547a3acd
jongja <whwhdgus94@naver.com>
2020-12-08 23:54:02 +0900

    Update Fatcalculator

--- PracticeApps/FatCalculator/app/src/main/java/com/example/fatcalculator/MainActivity.kt
+++ PracticeApps/FatCalculator/app/src/main/java/com/example/fatcalculator/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.fatcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rb = findViewById<Button>(R.id.resultButton)
+        val weight = findViewById<EditText>(R.id.weightEditText)
+        val height = findViewById<EditText>(R.id.heightEditText)
+
+       rb.setOnClickListener {
+            //val intent = Intent(this, ResultActivity::class.java)
+            //intent.putExtra("weight", weight.text.toString())
+            //intent.putExtra("height", height.text.toString())
+            //startActivity(intent)
+
+            startActivity<ResultActivity>( // with anko library
+                "weight" to weight.text.toString(),
+                "height" to height.text.toString()
+            )
+        }
     }
 }
--- PracticeApps/FatCalculator/app/src/main/java/com/example/fatcalculator/ResultActivity.kt
+++ PracticeApps/FatCalculator/app/src/main/java/com/example/fatcalculator/ResultActivity.kt
@@ -2,10 +2,34 @@ package com.example.fatcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val weight = intent.getStringExtra("weight")
+        val height = intent.getStringExtra("height")
+        val res = findViewById<TextView>(R.id.resultText)
+        val img = findViewById<ImageView>(R.id.imgView)
+
+        val bmi = 28
+
+//        when{
+//            bmi >= 35 -> res.text = "Highest fat.."
+//            bmi >= 30 -> res.text = "2 level fat.."
+//            bmi >= 25 -> res.text = "1 level fat"
+//            bmi >= 23 -> res.text = "High Weight"
+//            bmi >= 18.5 -> res.text = "Normal!"
+//            else -> res.text = "Low fat"
+//        }
+//
+//        when{
+//            bmi >= 23 -> img.setImageResource(R.drawable.ic_baseline_pan_tool_24)
+//            bmi >= 18.5 -> img.setImageResource(R.drawable.ic_baseline_mood_bad_24)
+//            else -> img.setImageResource(R.drawable.ic_baseline_emoji_emotions_24)
+//        }
     }
 }
