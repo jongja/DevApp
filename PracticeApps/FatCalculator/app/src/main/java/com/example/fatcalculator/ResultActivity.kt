a66831e2a24129b54da0342aba1e693150b2f0e9
jongja <whwhdgus94@naver.com>
2020-12-11 15:41:45 +0900

    Fix error

--- PracticeApps/FatCalculator/app/src/main/java/com/example/fatcalculator/ResultActivity.kt
+++ PracticeApps/FatCalculator/app/src/main/java/com/example/fatcalculator/ResultActivity.kt
@@ -10,26 +10,26 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        val weight = intent.getStringExtra("weight")
-        val height = intent.getStringExtra("height")
+        val weight = intent.getStringExtra("weight")!!.toInt()
+        val height = intent.getStringExtra("height")!!.toInt()
         val res = findViewById<TextView>(R.id.resultText)
         val img = findViewById<ImageView>(R.id.imgView)
 
-        val bmi = 28
+        val bmi = weight.div(Math.pow(height.div(100.0),2.0))
 
-//        when{
-//            bmi >= 35 -> res.text = "Highest fat.."
-//            bmi >= 30 -> res.text = "2 level fat.."
-//            bmi >= 25 -> res.text = "1 level fat"
-//            bmi >= 23 -> res.text = "High Weight"
-//            bmi >= 18.5 -> res.text = "Normal!"
-//            else -> res.text = "Low fat"
-//        }
-//
-//        when{
-//            bmi >= 23 -> img.setImageResource(R.drawable.ic_baseline_pan_tool_24)
-//            bmi >= 18.5 -> img.setImageResource(R.drawable.ic_baseline_mood_bad_24)
-//            else -> img.setImageResource(R.drawable.ic_baseline_emoji_emotions_24)
-//        }
+        when{
+            bmi >= 35 -> res.text = "Highest fat.."
+            bmi >= 30 -> res.text = "2 level fat.."
+            bmi >= 25 -> res.text = "1 level fat"
+            bmi >= 23 -> res.text = "High Weight"
+            bmi >= 18.5 -> res.text = "Normal!"
+            else -> res.text = "Low fat"
+        }
+
+        when{
+            bmi >= 23 -> img.setImageResource(R.drawable.ic_baseline_pan_tool_24)
+            bmi >= 18.5 -> img.setImageResource(R.drawable.ic_baseline_emoji_emotions_24)
+            else -> img.setImageResource(R.drawable.ic_baseline_mood_bad_24)
+        }
     }
 }
